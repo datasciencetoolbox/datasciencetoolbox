79f76c8d4e6e4cd1d5ef9e430e6fb2222affb80f
Jeroen Janssens <jeroen@jeroenjanssens.com>
2014-04-28 22:04:30 -0400

    Upgrade to Ubuntu 14.04 and add LXDE

--- build/build.sh
+++ build/build.sh
@@ -10,19 +10,17 @@ echo "AWS_REGION: ${AWS_REGION}"
 echo -n 'DST_URL: '
 echo $DST_URL | tee url
 
-sleep 5
+#sleep 5
 
 ## Update version of dst package and try to upload package to PyPi
 #< ../manager/setup.py.j2 sed "s/{{version}}/${DST_VERSION}/" > ../manager/setup.py
 #( cd ../manager; python setup.py sdist upload )
 
 
-
-
 #echo "Current boxes:"
 #aws s3api list-objects --bucket data-science-toolbox | jq '.Contents[].Key' | tr -d \"
 
-#echo "URL: ${DST_URL}"
+echo "URL: ${DST_URL}"
 
 rm -rf output-virtualbox-iso
 rm -rf packer_virtualbox-iso_virtualbox.box
@@ -31,10 +29,19 @@ rm -rf packer_virtualbox-iso_virtualbox.box
 echo "Build AMI and Vagrant with Packer"
 packer build -var-file=variables.json -var "dst_version=${DST_VERSION}" -only=virtualbox-iso dst.json 
 
+# virtualbox-iso
+# amazon-ebs
+
 
 ##2. Rename and upload Vagrant box to S3
-#echo "Rename and upload Vagrant box to S3"
+echo "Rename and upload Vagrant box to S3"
+mv packer_virtualbox-iso_virtualbox.box boxes/$DST_BOX #
+#aws s3 cp boxes/$DST_BOX s3://data-science-toolbox/$DST_BOX --acl public-read
 #mv packer_virtualbox-iso_virtualbox.box boxes/$DST_BOX && aws s3 cp boxes/$DST_BOX s3://data-science-toolbox/ --acl public-read
+
+###Update local Vagrant box
+vagrant box remove dst
+vagrant box add dst boxes/$DST_BOX
 
 
 #echo "Current boxes:"
@@ -43,12 +50,16 @@ packer build -var-file=variables.json -var "dst_version=${DST_VERSION}" -only=vi
 
 ##4. Make AMI public
 #AMI_ID=$(aws ec2 describe-images --owner self | jq '.Images[] | select(.Name=="dst-'${DST_VERSION}'") |  .ImageId' | tr -d \")
-#aws ec2 modify-image-attribute --image-id $AMI_ID --launch-permission "{\"Add\": [{\"Group\":\"all\"}]}"
+#echo "AMI_ID: ${AMI_ID}"
 #aws ec2 create-tags --resources $AMI_ID --tags Key=Name,Value='Data Science Toolbox'
-#aws ec2 copy-image --source-image-id $AMI_ID --source-region $AWS_REGION --region eu-west-1 --name "dst-${DST_VERSION}"
+#aws ec2 modify-image-attribute --image-id $AMI_ID --launch-permission "{\"Add\": [{\"Group\":\"all\"}]}"
+
+#aws ec2 copy-image --source-image-id $AMI_ID --source-region $AWS_REGION --region us-west-2 --name "dst-${DST_VERSION}"
+
+####aws ec2 copy-image --source-image-id $AMI_ID --source-region $AWS_REGION --region eu-west-1 --name "dst-${DST_VERSION}"
 
 
-##3. Copy AMI to other regions
+#3. Copy AMI to other regions
 #OTHER_REGIONS=$(aws ec2 describe-regions | jq '.Regions[].RegionName' | tr -d \" | grep -v $AWS_REGION)
 #for r in $OTHER_REGIONS; do
 	#echo "Copying AMI to region: $r"
@@ -56,14 +67,28 @@ packer build -var-file=variables.json -var "dst_version=${DST_VERSION}" -only=vi
 #done
 
 
-#5. Create static HTML
-#REGIONS=$(aws ec2 describe-regions | jq '.Regions[].RegionName' | tr -d \")
+#REGIONS=$(aws ec2 describe-regions | jq '.Regions[].RegionName' | tr -d \" | grep ap-southeast-2)
 #echo "region,ami_id" > amis.csv
 #for r in $REGIONS; do
-	#AMI_ID=$(aws ec2 describe-images --owner self --region $r | jq '.Images[] | select(.Name=="dst-'${DST_VERSION}'") | .ImageId' | tr -d \")
-	#echo "${r},${AMI_ID}" | tee -a amis.csv
+	#OTHER_AMI_ID=$(aws ec2 describe-images --owner self --region $r | jq '.Images[] | select(.Name=="dst-'${DST_VERSION}'") | .ImageId' | tr -d \")
+
+	#echo "${r},${OTHER_AMI_ID}" | tee -a amis.csv
+
+	#aws ec2 create-tags --resources $OTHER_AMI_ID --tags Key=Name,Value='Data Science Toolbox' --region $r
+	#aws ec2 modify-image-attribute --image-id $OTHER_AMI_ID --launch-permission "{\"Add\": [{\"Group\":\"all\"}]}" --region $r
+	#aws ec2 modify-image-attribute --image-id $OTHER_AMI_ID --description "Data Science Toolbox -- Start doing data science in minutes. Visit http://datasciencetoolbox.org for more information." --region $r
+	#echo 
+	#echo
 #done
 
+#5. Create static HTML
+#REGIONS=$(aws ec2 describe-regions | jq '.Regions[].RegionName' | tr -d \")
+#echo "region_code,region_name,ami_id" > amis.csv
+#for REGION_CODE in $REGIONS; do
+	#OTHER_AMI_ID=$(aws ec2 describe-images --owner self --region $REGION_CODE | jq '.Images[] | select(.Name=="dst-'${DST_VERSION}'") | .ImageId' | tr -d \")
+	#REGION_NAME=$(cat regions.csv | grep $REGION_CODE | cut -d, -f2)
+	#echo "${REGION_CODE},${REGION_NAME},${OTHER_AMI_ID}" | tee -a amis.csv
+#done
 
 # Deregister other AMIs
 #All AMIs registered using the image bundle must be de-registered using ec2-deregister.
